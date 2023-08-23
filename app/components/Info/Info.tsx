86fab6d53796390190b5ff32c2eed4786cf38006
rutvijjoshi26 <rutvijjoshi5@gmail.com>
2023-08-24 01:17:54 +0530

    Implemented information component

---
+++ app/components/Info/Info.tsx
@@ -0,0 +1,21 @@
+const Info = () => {
+  return (
+    <div className="flex flex-row gap-6 justify-center py-28">
+        <div className="relative min-w-[25vw]">
+            <div className="font-bold absolute top-2 text-3xl">
+            EQ beats IQ
+            </div>
+        </div>
+        <div className="max-w-[25vw] p-2 text-clip text-md">
+            People with high emotional intelligence (EQ) live more fulfilled lives. 
+            They tend to be happier and have healthier relationships.
+        </div>
+        <div className="max-w-[25vw] p-2 text-clip text-md">
+            They are more successful in their pursuits and make for inspiring leaders. 
+            According to science, they earn $29k a year.
+        </div>
+    </div>
+  )
+}
+
+export default Info
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero/Hero'
+import Info from './components/Info/Info'
 
 export default function Home() {
   return (
     <>
     <Hero/>
+    <Info/>
     </>
   )
 }
